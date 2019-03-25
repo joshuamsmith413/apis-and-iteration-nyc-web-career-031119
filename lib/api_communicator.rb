ed00691fa316bfdf9b077cd9e242ba5eef87da9f
joshuamsmith413 <joshuamsmith413@gmail.com>
2019-03-25 15:06:58 -0400

    I am nothing without Elaina

--- lib/api_communicator.rb
+++ lib/api_communicator.rb
@@ -6,6 +6,25 @@ def get_character_movies_from_api(character_name)
   #make the web request
   response_string = RestClient.get('http://www.swapi.co/api/people/')
   response_hash = JSON.parse(response_string)
+  data = JSON.parse(response_string.body)
+
+  name_of_character = data['results'].find do|list|
+      list["name"].downcase == character_name
+  end
+
+
+    film_string_array = name_of_character['films'].map do |url|
+      JSON.parse(RestClient.get(url))
+  end
+
+    film_list = film_string_array.map do |list|
+      list['title']
+    end
+
+
+
+
+
 
   # iterate over the response hash to find the collection of `films` for the given
   #   `character`
@@ -20,11 +39,16 @@ end
 
 def print_movies(films)
   # some iteration magic and puts out the movies in a nice list
+  films.each do |list|
+    puts list
+    puts "*" * 15
+  end
 end
 
 def show_character_movies(character)
   films = get_character_movies_from_api(character)
   print_movies(films)
+
 end
 
 ## BONUS
